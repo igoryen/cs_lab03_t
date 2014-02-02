cccc2197a9a085b089e8a7226adb91efcc7b5a27
igoryen <igor.yentaltsev@gmail.com>
2014-02-02 12:49:11 -0500

    ViewModels/Repo_Student.cs/getStudentsPublic(): changed

--- myAppMemory/myAppMemory/ViewModels/Repo_Student.cs
+++ myAppMemory/myAppMemory/ViewModels/Repo_Student.cs
@@ -39,7 +39,20 @@ namespace myAppMemory.ViewModels {
     }
 
     public IEnumerable<StudentPublic> getStudentsPublic() { // 1
+      var ls = Students.OrderBy(n => n.StudentNumber); // 20
+      List<StudentPublic> rls = new List<StudentPublic>(); // 25
 
+      foreach (var item in ls) {  // 30
+        StudentPublic row = new StudentPublic();   // 35
+        
+        row.StudentNumber = item.StudentNumber;  // 40 
+        row.FirstName = item.FirstName;
+        row.LastName = item.LastName;
+        row.Phone = item.Phone;
+        
+        rls.Add(row);  // 45 
+      }
+      return rls;  // 50
     }
 
     public IEnumerable<StudentFull> getStudentsFull() {
@@ -63,7 +76,16 @@ namespace myAppMemory.ViewModels {
     public List<Student> Students { get; set; }
   }
 }
-// getStudentsPublic() - gets a List of all Students, mapped to a List of StudentPublic objects, sorted by StudentNumber
+// getStudentsPublic()  -- gets a List of all Students, mapped to a List of StudentPublic objects, sorted by StudentNumber
+/* 20. make a list of students ordered by StudentNo
+ * 25. make an empty 4-column table "StudentPublic"
+ * 30. loop through the list of students ordered by StudentNo
+ * 35. make a 4-column row (StudentPublic)
+ * 40. fill out the row's 4 columns with data from students list
+ * 45. add the 4-column row StudentPublic to the 4-column table for StudentPublic's
+ * 50. produce the filled out 4-column table of StudentPublic's 
+ */
+ */
 
 // getStudentsFull() - gets a List of all Students, mapped to a List of StudentFull objects, sorted by LastName
 
